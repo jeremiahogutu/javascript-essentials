932c9eaeb68ecadd0ba316ff0cbf9f111321251d
Jeremiah Ogutu <jeremiahogutu@protonmail.com>
2017-08-04 23:17:27 -0400

    updated basic-function

--- basic-function/script.js
+++ basic-function/script.js
@@ -7,4 +7,8 @@ function findBiggestFraction(a,b) {
 var firstFraction = 3/4;
 var secondFraction = 5/7;
 
-console.log(findBiggestFraction(firstFraction, secondFraction));
+var fractionResult = findBiggestFraction(firstFraction, secondFraction);
+
+console.log("First fraction result: ", firstFraction);
+console.log("Second fraction result: ", secondFraction);
+console.log("Fraction "+ fractionResult[0] + " with a value of "+ fractionResult[1] + " is the biggest");
