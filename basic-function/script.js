f7bd376529907222e242f94e34e4354d4302a27f
Jeremiah Ogutu <jeremiahogutu@protonmail.com>
2017-08-04 23:09:13 -0400

    added basic function

---
+++ basic-function/script.js
@@ -0,0 +1,10 @@
+function findBiggestFraction(a,b) {
+	var result;
+	a>b ? result = ["firstFraction", a] : result = ["secondFraction", b];
+	return result;
+}
+
+var firstFraction = 3/4;
+var secondFraction = 5/7;
+
+console.log(findBiggestFraction(firstFraction, secondFraction));
